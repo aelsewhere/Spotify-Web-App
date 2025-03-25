cbe94fc59a93ac8c1e134598ac09dfbf454ef8b3
aelsewhere <68285208+aelsewhere@users.noreply.github.com>
2025-03-25 10:38:09 -0400

    Update secret.py
    remove personal info

--- scripts/secret.py
+++ scripts/secret.py
@@ -1,4 +1,9 @@
-CLIENT_ID = '2f1cf27ea0fd4c208e0754d29afd36d4'
-CLIENT_SECRET = "51a4dd8b0b4e45bbb73e1a237860d198"
-REDIRECT_URI = "http://127.0.0.1:5000/callback"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
+CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
+REDIRECT_URI = os.getenv('SPOTIFY_REDIRECT_URI', "http://127.0.0.1:5000/callback")
 SCOPE = 'user-library-read user-top-read user-read-recently-played user-read-playback-state user-modify-playback-state user-read-currently-playing'
