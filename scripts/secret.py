84d28f78b523ce37642ae482cae6e3d9b8f5de6e
aelsewhere <68285208+aelsewhere@users.noreply.github.com>
2025-03-25 10:29:15 -0400

    Create secret.py

---
+++ scripts/secret.py
@@ -0,0 +1,4 @@
+CLIENT_ID = '2f1cf27ea0fd4c208e0754d29afd36d4'
+CLIENT_SECRET = "51a4dd8b0b4e45bbb73e1a237860d198"
+REDIRECT_URI = "http://127.0.0.1:5000/callback"
+SCOPE = 'user-library-read user-top-read user-read-recently-played user-read-playback-state user-modify-playback-state user-read-currently-playing'
