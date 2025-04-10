903d1f28f196ec192ab55884cce897515822836e
aelsewhere <68285208+aelsewhere@users.noreply.github.com>
2025-04-10 09:24:12 -0400

    delete files

--- scripts/audio_features.py
+++
@@ -1,8 +0,0 @@
-import spotipy
-from spotipy import util
-from scripts.user_info import get_user
-
-def get_audio_features(sp):
-  track_uri = input("Enter the Spotify track URI: ")
-  features = sp.audio_features(track_uri)
-  return features
